42f1cd97aae9647e36d9c74a366f25efbac55017
fresiazr <fresiazr@hotmail.com>
2023-11-10 16:30:15 -0300

    Commits

--- Universidad-Etapa3/proyectouni/appuni/urls.py
+++ Universidad-Etapa3/proyectouni/appuni/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="myindex"),
+    path("", views.index, name="index"),
     path("alumno/<str:nombre_alumno>", views.alumno, name="alumno"),
     path("nuevo-alumno", views.nuevo_alumno, name="nuevo_alumno"),
     path("actualizar-alumno/<int:pk>", views.actualizar_alumno, name="actualizar_alumno"),
