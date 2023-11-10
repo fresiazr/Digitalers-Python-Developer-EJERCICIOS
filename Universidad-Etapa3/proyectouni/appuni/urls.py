2236e403125a1eddefad238f9d015b7e5a56e84d
fresiazr <fresiazr@hotmail.com>
2023-11-10 16:24:13 -0300

    Commits

--- Universidad-Etapa3/proyectouni/appuni/urls.py
+++ Universidad-Etapa3/proyectouni/appuni/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="myindex"),
-    #path("alumno<str>", views.nombre_alumno, name="alumno"),
-    #path("cursos", views.cursos, name="cursos"),
+    path("alumno/<str:nombre_alumno>", views.alumno, name="alumno"),
+    path("nuevo-alumno", views.nuevo_alumno, name="nuevo_alumno"),
+    path("actualizar-alumno/<int:pk>", views.actualizar_alumno, name="actualizar_alumno"),
+    path("eliminar-alumno/<int:pk>", views.eliminar_alumno, name="eliminar_alumno"),
 ]
--- Universidad-Etapa3/proyectouni/appuni/views.py
+++ Universidad-Etapa3/proyectouni/appuni/views.py
@@ -1,4 +1,47 @@
-from django.http import HttpResponse, JsonResponse, Http404, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.urls import reverse
+from django.http import Http404, HttpResponseRedirect
+from .models import Alumno 
+from . import forms
 
-# Create your views here.
+def index(request):
+    alumnos = Alumno.objects.all()
+    ctx = {"alumnos": alumnos}
+    return render(request, "appuni/index.html", ctx)
+
+def alumno(request, nombre_alumno):
+    try:
+        alumno = Alumno.objects.get(nombre=nombre_alumno)
+    except Alumno.DoesNotExist:
+        raise Http404    
+    ctx = {"alumno": alumno}
+    return render(request, "appuni/alumno.html", ctx)
+
+def nuevo_alumno(request):
+    if request.method == "POST":
+        form = forms.FormularioCurso(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+    else:    
+        form = forms.FormularioCurso()
+    ctx = { "form": form }
+    return render(request, "appuni/nuevo_alumno.html", ctx)
+
+
+def actualizar_alumno(request, pk):
+    alumno_actual = Alumno.objects.get(id=pk)
+    if request.method == "POST":
+        form = forms.FormularioCurso(request.POST, instance=alumno_actual)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = forms.FormularioCurso(instance=alumno_actual)
+    return render(request, 'appuni/actualizar_alumno.html', {'form': form})
+
+
+def eliminar_alumno(request, pk):
+    eliminar_alumno = Alumno.objects.get(id=pk)
+    eliminar_alumno.delete()
+    return HttpResponseRedirect(reverse("index"))
